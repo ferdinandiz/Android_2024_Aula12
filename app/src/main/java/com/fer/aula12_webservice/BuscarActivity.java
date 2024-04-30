209ed0eb1132dadca6ab9dbf547b76cf9d65ca34
ferdinandiz <80607754+ferdinandiz@users.noreply.github.com>
2024-04-30 12:01:23 -0300

    Feature: Aula 12 WebeService - Troca de componentes de botões para RelativeLayout com um texto e uma imagem - Função para inativar componente

--- app/src/main/java/com/fer/aula12_webservice/BuscarActivity.java
+++ app/src/main/java/com/fer/aula12_webservice/BuscarActivity.java
@@ -1,13 +1,11 @@
 package com.fer.aula12_webservice;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
